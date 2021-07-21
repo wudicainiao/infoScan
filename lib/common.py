0795e77b4275befe06f481b12e2393e40fac9f50
dacAIniao <820340571@qq.com>
2021-07-21 14:35:53 +0800

    Update common.py

--- lib/common.py
+++ lib/common.py
@@ -1,25 +1,58 @@
+import os
+import json
+import queue
+import openpyxl as ws
 from gevent import socket
 from lib.config import define
 
 
 
 
-def creat_xlsx():
+def creat_xlsx(q_results):
     if os.path.exists(define.filename) == False:
         s = 0
         wb = ws.Workbook()
         ws1 = wb.active
         if os.path.exists('out\\') == False:
             os.mkdir('out')
-        word=['ip','host','title','port','protocol']
+        word=['host','ip','title','ports_open','cidr','asn','org','addr','isp','cdn','url','waf(True为存在waf)']
         for i in word:
             s = s + 1
             ws1.cell(row =1,column = s,value = i)
         wb.save(define.filename)
         #wb.close()
-        print(define.RED+"[*]创建文件成功 %s"%define.filename)
+        q_results.put("[*]创建文件成功 %s"%define.filename)
     else:
-        print(define.RED+"[*]文件已存在 文件为:%s 请稍后运行程序"%define.filename)
+        q_results.put("[*]文件已存在 文件为:%s 请稍后运行程序"%define.filename)
+
+def write_xlsx(q_targets, q_results):
+    #first open file
+    wb = ws.load_workbook(define.filename)
+    q_results.put("[*]结果正在写入文件")
+    while True:
+        try:
+            target = q_targets.get(timeout=0.2)
+        except queue.Empty:
+            break
+    #second write row in memory
+        sheet1 = wb['Sheet']
+        num = sheet1.max_row
+        sheet1.cell(row = num+1,column = 1,value = target['host'])
+        sheet1.cell(row = num+1,column = 2,value = target['ip'])
+        sheet1.cell(row = num+1,column = 3,value = str(target['title']))
+        sheet1.cell(row = num+1,column = 4,value = str(target['ports_open']))
+        sheet1.cell(row = num+1,column = 5,value = target['cidr'])
+        sheet1.cell(row = num+1,column = 6,value = target['asn'])
+        sheet1.cell(row = num+1,column = 7,value = target['org'])
+        sheet1.cell(row = num+1,column = 8,value = target['addr'])
+        sheet1.cell(row = num+1,column = 9,value = target['isp'])
+        sheet1.cell(row = num+1,column = 10,value = target['cdn'])
+        sheet1.cell(row = num+1,column = 11,value = str(target['url']))
+        sheet1.cell(row = num+1,column = 12,value = str(target['waf']))
+    #thrid write and close
+    wb.save(define.filename)
+    wb.close()
+    q_results.put("[*]结果写入完成 : %s"%define.filename)
 
 def is_port_open(host, port):
     try:
