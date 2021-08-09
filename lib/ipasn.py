9eba0101ffaa2bfee91ba44b771d05904cc33602
dacAIniao <820340571@qq.com>
2021-08-09 15:47:06 +0800

    Update ipasn.py

--- lib/ipasn.py
+++ lib/ipasn.py
@@ -3,33 +3,31 @@ import queue
 import zipfile
 import sqlite3
 from ipaddress import IPv4Address
-
 from lib.config import define
 
 
-
-def get_db_path():
-    zip_path = define.data_storage_dir.joinpath('ip2location.zip')
-    db_path = define.data_storage_dir.joinpath('ip2location.db')
-    if db_path.exists():
-        return db_path
-    zf = zipfile.ZipFile(str(zip_path))
-    zf.extract('ip2location.db', define.data_storage_dir)
-    return db_path
-
-def ip_to_int(ip):
-    if isinstance(ip, int):
-        return ip
-    try:
-        ipv4 = IPv4Address(ip)
-    except Exception as e:
-        logger.log('ERROR', e.args)
-        return 0
-    return int(ipv4)
-
 class IPAsnInfo:
     def __init__(self):
-        self.path = get_db_path()
+        self.path = self.get_db_path()
+
+    def get_db_path(self):
+        zip_path = define.data_storage_dir.joinpath('ip2location.zip')
+        db_path = define.data_storage_dir.joinpath('ip2location.db')
+        if db_path.exists():
+            return db_path
+        zf = zipfile.ZipFile(str(zip_path))
+        zf.extract('ip2location.db', define.data_storage_dir)
+        return db_path
+
+    def ip_to_int(self, ip):
+        if isinstance(ip, int):
+            return ip
+        try:
+            ipv4 = IPv4Address(ip)
+        except Exception as e:
+            logger.log('ERROR', e.args)
+            return 0
+        return int(ipv4)
 
     def run(self, q_targets, q_targets_ex):
         conn = sqlite3.connect(self.path)
@@ -38,7 +36,7 @@ class IPAsnInfo:
         while True:
             try:
                 target = q_targets.get(timeout=0.2)
-                ip = ip_to_int(target['ip'])
+                ip = self.ip_to_int(target['ip'])
                 cursor = c.execute(f"SELECT * FROM asn WHERE ip_from <= {ip} AND ip_to >= {ip} LIMIT 1;")
                 for s in cursor:
                     target['cidr'] = s[2]
